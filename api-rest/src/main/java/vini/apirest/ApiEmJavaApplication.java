fc0dc6b684202d45482130d413d8c40c484df372
Vinícius Augusto <vinicius.augusto0704@gmail.com>
2022-05-31 23:33:47 -0300

    feature: Criação da API 1.0

---
+++ api-rest/src/main/java/vini/apirest/ApiEmJavaApplication.java
@@ -0,0 +1,13 @@
+package vini.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmJavaApplication.class, args);
+	}
+
+}
